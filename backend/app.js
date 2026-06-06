a7d7651f8c08901157e196d362acd2249c8299ba
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-07 00:26:00 +0900

    feat: 서버 가동 시 DB 연결 및 세팅

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const prefix = require("./Utils/prefix");
+const initDB = require("./Database/initDatabase");
 
 const app = express();
 
@@ -25,6 +26,18 @@ app.use(prefix.AUTH, routers[0]);
 
 // todo
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+// DB 연결 및 서버 가동
+const startServer = async () => {
+    try {
+        // DB 연결 완료될 때까지 대기
+        await initDB();
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+startServer();
