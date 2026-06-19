859c56c8ab43674db3311c7317db497a1755c0aa
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-20 02:46:50 +0900

    feat: postService todo 관련 내용 추가

--- backend/Service/postService.js
+++ backend/Service/postService.js
@@ -1,4 +1,5 @@
 const postRepository = require("../Repository/postRepository");
+const todoRepository = require("../Repository/todoRepository");
 
 const postService = {
     async getPostIts(user_id, page = 1, filter) {
@@ -6,7 +7,28 @@ const postService = {
             return await postRepository.findByUserId(user_id, page);
         }
         return await postRepository.findAll(page);
-    }
+    },
+
+    /**
+     * 이 아래 코드부터는 todoRepository 를 사용함.
+     */
+    async getTodos(post_it_id) {
+        return await todoRepository.findByPostItId(post_it_id);
+    },
+
+    async createTodo(content, due_date, post_it_id, user_id) {
+        const postIt = await postRepository.findById(post_it_id);
+
+        if (!postIt) {
+            return null;
+        }
+
+        if (Number(postIt.user_id) !== Number(user_id)) {
+            return null;
+        }
+
+        return await todoRepository.create(content, due_date, post_it_id);
+    },
 };
 
 module.exports = postService;
