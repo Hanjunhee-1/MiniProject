eed0e992f3334216baabbe40266f8f1238cdaf2b
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-20 02:46:25 +0900

    feat: postRoutes todo 관련 내용 추가

--- backend/Route/postRoutes.js
+++ backend/Route/postRoutes.js
@@ -6,6 +6,10 @@ const postController = require("../Controller/postController");
 
 const router = express.Router();
 
+
 router.get("/", authMiddleware, postController.getPostIts);
+router.get("/:postId", authMiddleware, postController.getTodos);
+router.post("/:postId", authMiddleware, postController.createTodo);
+
 
 module.exports = router;
