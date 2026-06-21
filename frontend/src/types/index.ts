de870b67abd104b1878d94aca24eedf1ef588f10
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-21 22:36:26 +0900

    feat: 데이터 모델을 통합하여 export

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,9 @@
+/**
+ * interface 는 type 과 다르게 속성을 추가하여 가지는 것이 가능.
+ * 확장성을 위해 type 이 아닌 interface 로 선언.
+ */
+
+// 통합하여 export
+export * from "./user";
+export * from "./todo";
+export * from "./postit";
