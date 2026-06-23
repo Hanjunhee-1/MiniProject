ec4995d39e0d2d499eb08ead92cfe4a0659ad399
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-23 19:48:45 +0900

    feat: 칠판 프레임 컴포넌트화

---
+++ frontend/src/components/dashboard/ChalkboardFrame.tsx
@@ -0,0 +1,18 @@
+import { ForwardedRef, RefObject } from "react";
+
+type ChalkboardFrameProps = {
+    ref?: ForwardedRef<HTMLDivElement>;
+    children: React.ReactNode;
+    className?: string;
+}
+
+export default function ChalkboardFrame({ children, className, ref }: ChalkboardFrameProps) {
+    return (
+        <div
+            ref={ref}
+            className={`w-full max-w-6xl h-[80vh] bg-[#234733] border-8 border-amber-900 rounded-lg shadow-2xl flex flex-col justify-between p-8 relative ${className ?? ""}`}
+        >
+            {children}
+        </div>
+    )
+}
