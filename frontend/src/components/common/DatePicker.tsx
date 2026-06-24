8150f911aeb383b426b25a75dddc0b77d73d9b39
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-24 15:21:12 +0900

    feat: DatePicker 컴포넌트 생성

---
+++ frontend/src/components/common/DatePicker.tsx
@@ -0,0 +1,29 @@
+import { ChangeEvent } from "react";
+
+type DatePickerProps = {
+    value?: string;
+    min?: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    className?: string;
+};
+
+export default function DatePicker({
+    value,
+    min,
+    onChange,
+    className
+}: DatePickerProps) {
+    return (
+        <input
+            type="date"
+            value={value}
+            min={min}
+            onChange={onChange}
+            className={
+                className
+                    ? className
+                    : `w-full bg-white/90 border border-slate-300 rounded p-1 text-[11px] font-mono outline-none`
+            }
+        />
+    )
+}
