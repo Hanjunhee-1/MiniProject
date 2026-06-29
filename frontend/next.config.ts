243880310454ffb835155ed8a7ade10962deb751
Hanjunhee-1 <hanjuni857@naver.com>
2026-06-29 17:16:35 +0900

    basePath 및 assetPrefix 설정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  basePath: "/skku",
+  assetPrefix: "/skku"
 };
 
 export default nextConfig;
